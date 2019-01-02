5ea7d4dd0c6f0e6b609b82d815952670a53ed4a0
gibeulah <gibeulah@microsoft.com>
2019-01-03 01:42:07 +0530

    First commit

--- TrappingRainWater/Program.cs
+++ TrappingRainWater/Program.cs
@@ -6,7 +6,56 @@ namespace TrappingRainWater
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Rain Water Trapping!");
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Enter the number of elements in the array");
+            try
+            {
+                int noElements = int.Parse(Console.ReadLine());
+                int[] array = new int[noElements];
+                Console.WriteLine("Enter the elements separated by space");
+                String[] elements = Console.ReadLine().Split(' ');
+                for (int i = 0; i < noElements; i++) {
+                    array[i] = int.Parse(elements[i]);
+                }
+                Console.WriteLine("Collected rain water is "+GetRainWater(array) +"units.");
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static int GetRainWater(int[] array) {
+            int result = 0;
+            //O(n) space for precomputing results
+            int[] left = new int[array.Length];
+            int[] right = new int[array.Length];
+
+            left[0] = array[0];
+            right[array.Length-1] = array[array.Length-1];
+
+            //Precomputing left and right max bars
+            for (int i = 1; i< array.Length; i++){
+                left[i] = Math.Max(left[i-1], array[i]);
+            }
+
+
+            for (int i = array.Length-2; i >=0; i--)
+            {
+                right[i] = Math.Max(right[i + 1], array[i]);
+            }
+
+
+            //Calculate water that every bar can hold
+            for (int i = 0; i < array.Length; i++) {
+                result += (Math.Min(left[i], right[i])) - array[i];
+            }
+
+            return result;
         }
     }
 }
